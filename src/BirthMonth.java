d47a27243fe5e119b7179f7f3e225077c9ed0ded
Ashamango <ashaanupdas@gmail.com>
2023-06-06 21:46:35 -0400

    Outputs the entered birth month if vaild (1-12) and displays an error message if not vaild.

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,8 +1,21 @@
 import java.util.Scanner;
 
-public class BirthMonth {
-    public static void main(String[] args) {
- 
+public class BirthMonth { // class BirthMonth
+    public static void main(String[] args) { //main
+        Scanner in = new Scanner(System.in);
 
-    }
-}
+        // Prompt and input
+        System.out.print("Enter your birth month [1-12 inclusive]:  "); // output "Enter your birth month [1-12 inclusive]: "
+        int birthMonth = in.nextInt(); // input birthMonth
+
+        if(birthMonth >=1 && birthMonth <= 12) //if birthMonth >=1 && <=12 then
+        {
+            System.out.print("Your birth month is: " + birthMonth); // output "Your birth month is: N "
+            in.nextLine();
+        }
+        else // else
+        {
+            System.out.print("You entered an incorrect birth month value: " + birthMonth); // output "You entered an incorrect birth month value: "
+        } // endif
+    } // return
+} // end class
